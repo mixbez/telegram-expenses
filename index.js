3f96a650ad204742edb3715e1f6e88c063c11747
mixbez <bezverhiymix@gmail.com>
2025-08-18 21:57:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
